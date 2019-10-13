53b890bd512a6f6f9de50a5a33b01c2351fbcfbf
Vlad340 <abcd-mail@yandex.ru>
2019-10-13 21:38:45 +0300

    deleted unused metods in 'Tariff'

--- src/main/java/ru/itpark/amountTrip/Tariff.java
+++ src/main/java/ru/itpark/amountTrip/Tariff.java
@@ -14,51 +14,5 @@ public class Tariff {
         this.discount = 5;
         this.maxAmountDiscount = 100_00L;
     }
-    public Tariff(long landingPrice, long tripPrice, long amountToCalculateDiscount, int discount, long maxAmountDiscount) {
-        this.landingPrice = landingPrice;
-        this.tripPrice = tripPrice;
-        this.amountToCalculateDiscount = amountToCalculateDiscount;
-        this.discount = discount;
-        this.maxAmountDiscount = maxAmountDiscount;
-    }
 
-    public long getLandingPrice() {
-        return landingPrice;
-    }
-
-    public void setLandingPrice(long landingPrice) {
-        this.landingPrice = landingPrice;
-    }
-
-    public long getTripPrice() {
-        return tripPrice;
-    }
-
-    public void setTripPrice(long tripPrice) {
-        this.tripPrice = tripPrice;
-    }
-
-    public long getAmountToCalculateDiscount() {
-        return amountToCalculateDiscount;
-    }
-
-    public void setAmountToCalculateDiscount(long amountToCalculateDiscount) {
-        this.amountToCalculateDiscount = amountToCalculateDiscount;
-    }
-
-    public int getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(int discount) {
-        this.discount = discount;
-    }
-
-    public long getMaxAmountDiscount() {
-        return maxAmountDiscount;
-    }
-
-    public void setMaxAmountDiscount(long maxAmountDiscount) {
-        this.maxAmountDiscount = maxAmountDiscount;
-    }
 }
